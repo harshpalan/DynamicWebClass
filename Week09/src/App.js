425919d2a5f61ef25fe3e6c245182f0a1e4153f2
Harsh Palan <harshpalan14@gmail.com>
2023-11-07 16:40:01 -0500

    add link to card

--- Week09/src/App.js
+++ Week09/src/App.js
@@ -1,16 +1,15 @@
-import Link from './components/Link';
-import CardPage from './pages/CardPage';
+import Link from "./components/Link";
 
 export default function App() {
   return (
     <div>
       <div>
-      <Link to="/button">Button</Link>
+        <Link to="/button">Button</Link>
       </div>
       <div>
-      <Link to="/accordion">Accordion</Link>
+        <Link to="/accordion">Accordion</Link>
       </div>
-      <CardPage />
+      <Link to="/card">Card</Link>
     </div>
   );
 }
